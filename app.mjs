33a11ec89a806ab8a49ee789b5b260ffcd2fa092
antonkrutikov <kap@reso-med.com>
2022-03-17 12:37:46 +0300

    to jpeg

--- app.mjs
+++ app.mjs
@@ -2,6 +2,11 @@ import fs from 'fs'
 import sharp from 'sharp'
 
 const inputFileName = 'sample.png'
-const outputFileName = 'sample_out.png'
+const outputFileName = 'sample_out.jpeg'
 const file = fs.readFileSync(inputFileName)
-sharp(file).resize(900,900,{fit: 'inside'}).toFile(outputFileName)
+sharp(file)
+.resize(900,900,{fit: 'inside'})
+.jpeg({
+    quality: 80
+})
+.toFile(outputFileName)
